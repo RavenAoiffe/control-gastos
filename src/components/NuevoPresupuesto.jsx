07daed3cdf971902679d5a937d2b6418fcdefe63
Ameyalli <ameyanezcmm@gmail.com>
2023-07-23 22:22:06 -0600

    Nuevo presupuesto formulario

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 const NuevoPresupuesto = () =>{
     return(
-        <>
-        <h2>Nuevo presupuesto</h2>
-        </>
+        <div className="contenedor-presupuesto contenedor sombra">
+            <form action="#" className="formulario">
+                <div className="campo">
+                    <label htmlFor="nuevo-presupuesto">Definir presupuesto</label>
+                    <input type="text"
+                    className="nuevo-presupuesto" 
+                    placeholder="añade tu presupuesto"
+                    name="nuevo-presupuesto"/>
+                </div>
+                <input type="submit" value="Añadir" />
+            </form>
+        </div>
     )
 }
 
-export default Header;
+export default NuevoPresupuesto;
