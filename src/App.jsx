e717bbc705f876086254884367f1756e3304b694
Ameyalli Yáñez <ravename@hotmail.com>
2023-07-25 13:32:06 -0600

    Update App.jsx
    Validar si el presupuesto es válido

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Header from './components/Header';
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
 
+  const [isValidPresupuesto, setisValidPresupuesto] = useState(false);
+
   return (
     <>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
+        setisValidPresupuesto={setisValidPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
       />
     </>
   )
