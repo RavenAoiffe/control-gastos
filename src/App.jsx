c213a28e4d60895f82397e5df8d05db0e7b2ac2c
Ameyalli Yáñez <ravename@hotmail.com>
2023-08-30 12:45:57 -0600

    Update App.jsx
    Se mandó la fecha a Gasto y el id

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Modal from './components/Modal';
+import ListadoGastos from './components/ListadoGastos';
+
+import {generarId}  from './helpers';
+
 import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
@@ -23,11 +27,20 @@ function App() {
   }
 
   const guardarGasto = gasto =>{
-    setGastos(gasto)
+    gasto.id = generarId();
+    gasto.fecha = Date.now();
+    setGastos([...gastos,gasto]);
+
+    setanimarModal(false);
+
+    setTimeout(() => {
+        setModal(false);
+    }, 500)
+
   }
 
   return (
-    <>
+    <div className={modal ? 'fijar': ''}>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
@@ -36,7 +49,12 @@ function App() {
       />
 
       {isValidPresupuesto &&
-        (
+       <>
+          <main>
+            <ListadoGastos
+            gastos={gastos}
+            />
+          </main>
           <div className="nuevo-gasto">
             <img
               src={IconoNuevoGasto}
@@ -44,7 +62,7 @@ function App() {
               onClick={handleNuevoGasto}
             />
           </div>
-        ) 
+        </>
       }
 
       {modal && (
@@ -55,7 +73,7 @@ function App() {
           guardarGasto={guardarGasto}
         />
       )}
-    </>
+    </div>
   )
 }
 
