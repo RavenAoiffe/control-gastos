0da08498b91eb33a7e256881b3f1738c9f500d6b
Ameyalli Yáñez <ravename@hotmail.com>
2023-08-31 14:05:25 -0600

    Update ControlPresupuesto.jsx
    totalgastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,5 +1,20 @@
-const ControlPresupuesto = ({presupuesto}) => {
+import { useEffect, useState } from "react";
+
+const ControlPresupuesto = ({presupuesto,gastos}) => {
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
+
     
+    useEffect(()=>{
+        
+        const totalGastado = gastos.reduce((total,gasto)=> gasto.cantidad + total, 0);
+        const totalDisponible = presupuesto - totalGastado;
+
+        setGastado(totalGastado)
+        setDisponible(totalDisponible)
+
+    }, [gastos])
+
     const formatearCantidad = (cantidad) => {
         return cantidad.toLocaleString('en-US', {
             style: 'currency',
@@ -17,10 +32,10 @@ const ControlPresupuesto = ({presupuesto}) => {
                         <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
                     </p>
                     <p> 
-                        <span>Disponible: </span> $0.00
+                        <span>Disponible: </span> {formatearCantidad(disponible)}
                     </p>
                     <p> 
-                        <span>Gastado: </span> $0.00
+                        <span>Gastado: </span> {formatearCantidad(gastado)}
                     </p>
                 </div>
             </div>
