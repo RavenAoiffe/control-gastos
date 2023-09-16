5a7c5e339ca382454a1a7316a2e0c7a9ccfb1960
Ameyalli <ameyanezcmm@gmail.com>
2023-09-16 17:10:25 -0600

    Cambiar color si se pasa el presupuesto

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -39,9 +39,9 @@ const ControlPresupuesto = ({presupuesto,gastos}) => {
             <div>
                 <CircularProgressbar
                     styles={buildStyles({
-                        pathColor:'#3b82f6',
                         trailColor:'#f5f5f5',
-                        textColor:'#3b82f6'
+                        pathColor: porcentaje > 100 ? '#dc2626' : '#3b82f6',
+                        textColor: porcentaje > 100 ? '#dc2626' : '#3b82f6'
                     })}
                     value={porcentaje}
                     text={`${porcentaje}% gastado`}
@@ -51,7 +51,7 @@ const ControlPresupuesto = ({presupuesto,gastos}) => {
                     <p> 
                         <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
                     </p>
-                    <p> 
+                    <p className={`${disponible < 0 ? 'negativo' : ''}`}> 
                         <span>Disponible: </span> {formatearCantidad(disponible)}
                     </p>
                     <p> 
